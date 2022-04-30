14fc6c330d9a2f0cc56c07dbb52a06226bf121e4
raynayx <raynayx@gmail.com>
2022-04-30 15:43:45 +0000

    week 3 cli try

--- week_3/xiao_rp2040/src/main.c
+++ week_3/xiao_rp2040/src/main.c
@@ -12,8 +12,6 @@ void toggle(uint8_t pin);
 
 const uint8_t LED = 25U;     // LED connected to GPIO 25
 
-const uint8_t NEOPWR = 11U;
-const uint8_t NEOPIXEL = 12U;
 
 cli c;
 char sw[256];
@@ -29,22 +27,13 @@ int main()
     gpio_init(LED);  //initialiaze GPIO
     // gpio_init(btn);
 
-    gpio_init(NEOPWR);
-    gpio_init(NEOPIXEL);
+  
 
     gpio_set_dir(LED,true); //set pin to output mode
     // gpio_set_dir(btn,false); //set pin to input mode
     // gpio_set_pulls(btn,true,false);     //enable pull up on btn pin
 
     // gpio_set_irq_enabled_with_callback(btn,GPIO_IRQ_LEVEL_LOW,true,&toggle);    //setup interrupt on specified GPIO
-    gpio_set_dir(NEOPWR,true);
-    gpio_set_dir(NEOPIXEL,true);
-    // gpio_set_pulls(NEOPWR,true,false);
-
-    gpio_put(NEOPIXEL,true);
-    sleep_us(0.4);
-    gpio_put(NEOPIXEL,false);
-    sleep_us(0.8);
 
     showDeviceInfo(&c);
     while(1)
