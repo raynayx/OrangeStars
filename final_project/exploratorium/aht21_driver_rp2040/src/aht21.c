3580b6c7eb455bc19c431af98c1f7b047a6df22b
raynayx <raynayx@gmail.com>
2022-05-24 15:07:05 +0000

    aht21 driver working

--- final_project/exploratorium/aht21_driver_rp2040/src/aht21.c
+++ final_project/exploratorium/aht21_driver_rp2040/src/aht21.c
@@ -1,313 +1,63 @@
 #include "aht21.h"
 #include <stdio.h>
 
-//#include <math.h>
+uint8_t sensor_calibrate_cmd[3] = {0xE1,0x08,0x00};
+uint8_t sensor_normal_cmd[3] = {0xA8,0x00,0x00};
+uint8_t sensor_measure_cmd[3] = {0xAC,0x33,0x00};
+uint8_t sensor_reset_cmd = 0xBA;
+bool get_r_humidity_cmd = true;
+bool get_temperature_cmd = false;
 
-static uint8_t _get_busy(AHT21 *s, bool readAHT);
-static uint8_t _get_calibration(AHT21 *s);
-static bool _set_init_reg(AHT21 *s,uint8_t val);
-static bool _checkCRC8(AHT21* s);
-static void _read_measurement(AHT21* s);
 
-
-eAHT21_STATUS AHT21_init(AHT21 *s, uint16_t addr,i2c_inst_t *i2c)
+static void _read_sensor(AHT21 *s)
 {
-	s->addr = addr;
+	i2c_write_blocking(s->hal_i2c,s->addr,sensor_measure_cmd,3,false);
+	sleep_ms(100);
+
+	i2c_read_blocking(s->hal_i2c,s->addr,s->raw_resp,6,true);
+	i2c_read_blocking(s->hal_i2c,s->addr,&s->crc,1,false);
+}
+
+bool AHT21_init(AHT21 *s,i2c_inst_t *i2c)
+{
+	s->addr = AHT21_I2C_ADDR;
 	s->hal_i2c = i2c;
-	sleep_ms(AHT21_POWER_ON_DELAY);	//wait for power up
-	return AHT21_softreset(s);
 
+	i2c_write_blocking(s->hal_i2c,s->addr,sensor_calibrate_cmd,3,false);
+
+	if((AHT21_read_status(s)&0x18) == 0x18)
+	{
+		return true;
+	}
+	return false;
 }
 
-eAHT21_STATUS AHT21_softreset(AHT21 *s)
+uint8_t AHT21_read_status(AHT21 *s)
 {
-	// _read_status_reg(s);
+	i2c_read_blocking(s->hal_i2c,s->addr,&s->status,1,false);
 
-	i2c_write_blocking(s->hal_i2c,s->addr,(uint8_t*)AHT21_REG_SOFTRESET,1,false);
-
-	// if(res == PICO_ERROR_GENERIC )
-	// {
-	// 	return eAHT21_ERROR;
-	// }
-	sleep_ms(AHT21_SOFT_RESET_DELAY);
-
-	if((set_normal_mode(s) == true) && (_get_calibration(s) == AHT21_STATUS_CTRL_CAL_ON)); 
-	{
-		return eAHT21_OKAY;
-	}
-	return eAHT21_ERROR;
-
+	return s->status;
 }
 
-bool set_normal_mode(AHT21 *s)
+void AHT21_reset(AHT21 *s)
 {
-	return _set_init_reg(s,AHT21_INIT_CTRL_CAL_ON | AHT21_INIT_CTRL_NORMAL_MODE);
+	i2c_write_blocking(s->hal_i2c,s->addr,&sensor_reset_cmd,1,false);
 }
 
-static uint8_t _get_calibration(AHT21 *s)
+float AHT21_get_humidity(AHT21 *s)
 {
-	uint8_t val = _read_status_reg(s);
-	if (val != eAHT21_ERROR)
-	{
-		return (val & AHT21_STATUS_CTRL_CAL_ON); 
-	}
-	return eAHT21_ERROR;
-}
-
-static bool _set_init_reg(AHT21 *s,uint8_t val)
-{
-	sleep_ms(AHT21_CMD_DELAY);
-	int res;
-	i2c_write_blocking(s->hal_i2c,s->addr,(uint8_t *) AHT21_REG_INIT,1,true);
-	i2c_write_blocking(s->hal_i2c,s->addr,&val,1,true);
-	res = i2c_write_blocking(s->hal_i2c,s->addr,(uint8_t*)AHT21_INIT_CTRL_NOP,1,false);
-
-	return (res > 0);
-}
-
-
-uint8_t _read_status_reg(AHT21 *s)
-{
-	
-
-	uint8_t return_val;
-
-	i2c_write_blocking(s->hal_i2c,s->addr,(uint8_t*)AHT21_REG_STATUS,1,false);
-	sleep_ms(AHT21_CMD_DELAY);
-	// if(res == PICO_ERROR_GENERIC)
-	// {
-	// 	return eAHT21_ERROR;
-	// }
-
-	if(i2c_get_read_available(s->hal_i2c) == 0)
-	{
-		return eAHT21_ERROR;
-	}
-
-	i2c_read_blocking(s->hal_i2c,s->addr,&return_val,1,false);
-
-	// if(res == PICO_ERROR_GENERIC)
-	// {
-	// 	return eAHT21_ERROR;
-	// }
-	return return_val;
-}
-
-
-
-/**************************************************************************/
-/*
-    _checkCRC8()
-
-    Check CRC-8-Maxim of AHT2X measured data
-
-    NOTE:
-    - part of "readRawMeasurement()" function!!!
-    - only AHT2x sensors have CRC
-    - initial value=0xFF, polynomial=(x8 + x5 + x4 + 1) ie 0x31 CRC [7:0] = 1+X4+X5+X8
-*/
-/**************************************************************************/
-static bool _checkCRC8(AHT21* s)
-{
-	uint8_t crc = 0xFF;
-
-	for(uint8_t byteIndex = 0; byteIndex < 6; byteIndex++)
-	{
-		crc ^= s->raw_response[byteIndex];
-
-		for(uint8_t bitIndex = 8; bitIndex > 0; --bitIndex)
-		{
-			if(crc & 0x80)
-			{
-				crc = (crc << 1) ^ 0x31;
-			}
-			else
-			{
-				crc = (crc << 1);
-			}
-		}
-	}
-	return (crc == s->crc);
-}
-
-
-/**************************************************************************/
-/*
-    _readMeasurement()
-
-    Start new measurement, read sensor data to buffer & collect errors
-
-    NOTE:
-    - sensors data structure:
-      - {status, RH, RH, RH+T, T, T, CRC*}, *CRC for AHT2x only & for
-        status description see "_readStatusRegister()" NOTE
-*/
-/**************************************************************************/
-void _read_measurement(AHT21* s)
-{
-	i2c_write_blocking(s->hal_i2c,s->addr,(uint8_t*)AHT21_REG_MEASURE,1,true);
-	i2c_write_blocking(s->hal_i2c,s->addr,(uint8_t*)AHT21_START_MEASURE_CTRL,1,true);
-	int res = i2c_write_blocking(s->hal_i2c,s->addr,(uint8_t*)AHT21_START_MEASURE_CTRL_NOP,1,false);
-	
-	// if(res == PICO_ERROR_GENERIC)
-	// {
-	// 	s->status = eAHT21_ERROR;
-	// 	return;
-	// }
-	s->status = _get_busy(s,AHT21_FORCE_READ_DATA);
-	if(s->status == eAHT21_BUSY)
-	{
-		sleep_ms(AHT21_MEASUREMENT_DELAY - AHT21_CMD_DELAY);
-	}
-	else if(s->status != eAHT21_OKAY)
-	{
-		return;
-	}
-
-	res = i2c_get_read_available(s->hal_i2c);
-
-	if(res > 0)
-	{
-		i2c_read_blocking(s->hal_i2c,s->addr,s->raw_response,6,true);
-		i2c_read_blocking(s->hal_i2c,s->addr,&s->crc,1,false);
-	}
-	else
-	{
-		s->status = eAHT21_ERROR;
-		return;
-	}
-	s->status = _get_busy(s,AHT21_USE_READ_DATA);
-
-	if(s->status != eAHT21_OKAY)
-	{
-		return;
-	}
-	// check on CRC8 calculation
-	if(_checkCRC8(s) != true)
-	{
-		s->status = eAHT21_CRC8_ERROR; 
-	}
-}
-
-/**************************************************************************/
-/*
-    _getBusy()
-
-    Read/check busy bit after measurement command
-
-    NOTE:
-    - part of "readRawMeasurement()" function!!!
-    - 0x80=busy, 0x00=measurement completed, etc
-*/
-/**************************************************************************/
-static uint8_t _get_busy(AHT21 *s, bool readAHT)
-{
-	if(readAHT)
-	{
-		sleep_ms(AHT21_CMD_DELAY);
-
-		i2c_read_blocking(s->hal_i2c,s->addr,s->raw_response,1,false);
-	}
-
-	if((s->raw_response[0] & AHT21_STATUS_CTRL_BUSY) == AHT21_STATUS_CTRL_BUSY)
-	{
-		s->status = eAHT21_BUSY;
-	}
-	else
-	{
-		return s->status;
-	}
-}
-
-
-/**************************************************************************/
-/*
-    read_temperature()
-
-    Read temperature, in C 
-
-    NOTE:
-    - temperature range........ -40C..+85C
-    - temperature resolution... 0.01C
-    - temperature accuracy..... +-0.3C
-    - response time............ 5..30sec
-    - measurement with high frequency leads to heating of the
-      sensor, must be > 2 seconds apart to keep self-heating below 0.1C
-
-    - sensors data structure:
-      - {status, RH, RH, RH+T, T, T, CRC*}, *CRC for AHT2x only
-*/
-/**************************************************************************/
-float AHT21_read_temperature(AHT21* s,bool readAHT)
-{
-	if(readAHT == AHT21_FORCE_READ_DATA)
-	{
-		_read_measurement(s);
-	}
-	if (s->status != eAHT21_OKAY)
-	{
-		return eAHT21_ERROR;
-	}
-
-	int32_t temp = s->raw_response[3] & 0x0F;
-	temp <<= 8;
-	temp |= s->raw_response[4];
-	temp <<= 8;
-	temp |= s->raw_response[5];
-
-	s->temperature = ((float)((temp /AHT21_DENOM) * 200 - 50));
-	return s->temperature;
-}
-
-
-/**************************************************************************/
-/*
-    readHumidity()
-
-    Read relative humidity, in %
-
-    NOTE:
-    - relative humidity range........ 0%..100%
-    - relative humidity resolution... 0.024%
-    - relative humidity accuracy..... +-2%
-    - response time............ 5..30sec
-    - measurement with high frequency leads to heating of the
-      sensor, must be > 2 seconds apart to keep self-heating below 0.1C
-    - long-term exposure for 60 hours outside the normal range
-      (humidity > 80%) can lead to a temporary drift of the
-      signal +3%, sensor slowly returns to the calibrated state at normal
-      operating conditions
-
-    - sensors data structure:
-      - {status, RH, RH, RH+T, T, T, CRC*}, *CRC for AHT2x only
-
-    - normal operating range T -20C..+60C, RH 10%..80%
-    - maximum operating rage T -40C..+80C, RH 0%..100%
-*/
-/**************************************************************************/
-float AHT21_read_humidity(AHT21 *s,bool readAHT)
-{
-	if(readAHT == AHT21_FORCE_READ_DATA)
-	{
-		_read_measurement(s);
-	}
-	if(s->status != eAHT21_OKAY)
-	{
-		return eAHT21_ERROR;
-	}
-
-	uint32_t humidity = s->raw_response[1];
-	humidity <<= 8;
-	humidity |= s->raw_response[2];
-	humidity <<= 4;
-	humidity |= s->raw_response[3] >> 4;
-
-	if(humidity > 0x100000)
-	{
-		humidity = 0x100000;
-	}
-
-	s->humidity = ((float) (humidity/0x100000) * 100);
-
+	uint32_t raw_res;
+	_read_sensor(s);
+	raw_res = ((s->raw_resp[1] << 16) | (s->raw_resp[2] << 8 )| s->raw_resp[3]) >> 4;
+	s->humidity = ((raw_res * 100)/AHT21_DENOM);
 	return s->humidity;
+}
+
+float AHT21_get_temperature(AHT21 *s)
+{
+	uint32_t raw_res;
+	_read_sensor(s);
+	raw_res = (s->raw_resp[3] & 0x0F) << 16 | (s->raw_resp[4] << 8) | s->raw_resp[5];
+	s->temperature = (((200 * raw_res)/AHT21_DENOM) - 50);
+	return s->temperature;
 }
--- final_project/exploratorium/aht21_driver_rp2040/src/main.c
+++ final_project/exploratorium/aht21_driver_rp2040/src/main.c
@@ -18,7 +18,7 @@ int main()
 	gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
 	sleep_ms(5000);
-	while(AHT21_init(&s, AHT21_I2C_ADDR, i2c0) != eAHT21_OKAY)
+	while(!AHT21_init(&s, i2c0))
 	{
 		printf("AHT21:Init failed\n");
 		sleep_ms(5000);
@@ -30,15 +30,10 @@ int main()
 		sleep_ms(500);
 		gpio_put(LED, false);
 		sleep_ms(500);
+		AHT21_get_temperature(&s);
 
-		// float t = AHT21_read_temperature(&s, AHT21_FORCE_READ_DATA);
-		// float h = AHT21_read_humidity(&s,AHT21_USE_READ_DATA);
-		// printf("Temp:%.2fC\tHum:%.2f\n", t, h);
-
-		_read_status_reg(&s);
-
-		sleep_ms(1000);
-		printf("\nStatus: 0x%x\n", s.status);
+		printf("Temp:\t%.2fC and Humi:\t%.2f\n",s.temperature,s.humidity);
+		sleep_ms(2000);
 	}
 
 	return 0;
